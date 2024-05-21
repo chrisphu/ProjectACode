b3641715aaac07454fc850b81b304b89de6c75a1
Chris Phu <chrisnguyenphu@gmail.com>
2024-05-20 23:25:13 -0500

    Added smoother player controls by separating out forward/backward and left/right movement axes and their lerps. Also added smoother camera tracking.

--- Scripts/AttachedToScenes/Camera.cs
+++ Scripts/AttachedToScenes/Camera.cs
@@ -1,37 +1,63 @@
 using Godot;
 using System;
 
-namespace ProjectA
+namespace ProjectA;
+
+public partial class Camera : Camera3D
 {
-	public partial class Camera : Camera3D
-	{
-		[Export] private Node3D _objectToFollow;
-		[Export] private Vector3 _positionOffset;
-		[Export(PropertyHint.Range, "0.0f,1.0f")] private float _positionSmoothness = 0.995f;
-	
-		/// <summary>
-		/// Calls camera lerping method every frame.
-		/// </summary>
-		public override void _Process(double delta)
-		{
-			LerpCameraToDesiredPosition(delta);
-		}
+    [Export] private Node3D _trackedObject;
+    [Export] private Vector3 _cameraPositionOffset;
+    [Export] private Vector3 _lookingAtOffset;
+    [Export(PropertyHint.Range, "0.0f, 1.0f")] private float _cameraPositionSmoothness = 0.995f;
+    [Export(PropertyHint.Range, "0.0f, 1.0f")] private float _verticalTrackingSmoothness = 0.9f;
 
-		/// <summary>
-		/// Lerps camera to desired position with damped factor independent of frame rate.
-		/// </summary>
-		private void LerpCameraToDesiredPosition(double delta)
-		{
-			if (_objectToFollow == null)
-			{
-				return;
-			}
+    private float _trackedObjectVerticalPosition;
+    
+    public override void _Ready()
+    {
+        if (_trackedObject == null)
+        {
+            return;
+        }
 
-			Vector3 desiredPosition = _objectToFollow.Position + _positionOffset;
+        _trackedObjectVerticalPosition = _trackedObject.Position.Y;
+    }
+    
+    /// <summary>
+    /// Calls camera lerping method every frame.
+    /// </summary>
+    public override void _Process(double delta)
+    {
+        if (_trackedObject == null)
+        {
+            return;
+        }
+        
+        LerpCameraToDesiredPosition(delta);
+        UpdateCameraLookingAt(delta);
+    }
 
-			Position = Position.Lerp(
-				desiredPosition,
-				DampExtensionFunctions.DampFactorForLerp(_positionSmoothness, delta));
-		}
-	}
+    /// <summary>
+    /// Lerps camera to desired position with damped factor independent of frame rate.
+    /// </summary>
+    private void LerpCameraToDesiredPosition(double delta)
+    {
+        Vector3 desiredPosition = _trackedObject.Position + _trackedObject.Basis * _cameraPositionOffset;
+
+        Position = Position.Lerp(
+            desiredPosition,
+            FloatExtensionMethods.DampFactorForLerp(_cameraPositionSmoothness, delta));
+    }
+
+    private void UpdateCameraLookingAt(double delta)
+    {
+        _trackedObjectVerticalPosition = _trackedObjectVerticalPosition.Lerp(
+            _trackedObject.Position.Y,
+            FloatExtensionMethods.DampFactorForLerp(_verticalTrackingSmoothness, delta));
+        
+        Transform = Transform.LookingAt( new Vector3(
+            _trackedObject.Position.X,
+            _trackedObjectVerticalPosition,
+            _trackedObject.Position.Z) + _trackedObject.Basis * _lookingAtOffset);
+    }
 }
--- Scripts/AttachedToScenes/MainCharacter.cs
+++ Scripts/AttachedToScenes/MainCharacter.cs
@@ -1,65 +1,92 @@
 using Godot;
 using System;
 
-namespace ProjectA
+namespace ProjectA;
+
+public partial class MainCharacter : CharacterBody3D
 {
-	public partial class MainCharacter : CharacterBody3D
+	[Export] private MeshInstance3D _debugIndicator;
+	[Export] private float _maxMovementSpeed = 1.0f;
+	[Export(PropertyHint.Range, "0.0f, 1.0f")] private float _movementSmoothing = 0.99f;
+	[Export] private float _rotationSpeed = 0.5f;
+	
+	private Vector2 _relativeMouseMovement;
+	private float _forwardBackwardMovementSpeed;
+	private float _leftRightMovementSpeed;
+
+	/// <summary>
+	/// Sets mouse mode to being captured when scene starts.
+	/// </summary>
+	public override void _Ready()
 	{
-		[Export] private float _movementSpeed = 600.0f;
-		[Export] private float _rotationSpeed = 0.5f;
-
-		private Vector2 _relativeMouseMovement;
-
-		/// <summary>
-		/// Sets mouse mode to being captured when scene starts.
-		/// </summary>
-		public override void _Ready()
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
+	
+	/// <summary>
+	/// Called when any input is detected and stores relative mouse movement from event arguments.
+	/// </summary>
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion eventArguments)
 		{
-			Input.MouseMode = Input.MouseModeEnum.Captured;
+			_relativeMouseMovement = eventArguments.Relative;
 		}
+	}
+	
+	/// <summary>
+	/// Calls movement and rotation methods every frame.
+	/// </summary>
+	public override void _Process(double delta)
+	{
+		RotateMainCharacter(delta);
+		MoveMainCharacter(delta);
+	}
+
+	/// <summary>
+	/// Rotates main character based on last stored relative mouse moment and rotation speed.
+	/// </summary>
+	private void RotateMainCharacter(double delta)
+	{
+		Rotate(Transform.Basis.Y, -_relativeMouseMovement.X * _rotationSpeed * (float)delta);
 		
-		/// <summary>
-		/// Called when any input is detected and stores relative mouse movement from event arguments.
-		/// </summary>
-		public override void _Input(InputEvent @event)
-		{
-			if (@event is InputEventMouseMotion eventArguments)
-			{
-				_relativeMouseMovement = eventArguments.Relative;
-			}
-		}
+		// Needs to be reset as _Input otherwise will never set it to (0.0f, 0.0f).
+		_relativeMouseMovement = new Vector2();
+	}
+
+	/// <summary>
+	/// Moves main character based on input map.
+	/// </summary>
+	private void MoveMainCharacter(double delta)
+	{
+		float forwardBackwardMovement = Input.GetAxis("MoveBackward", "MoveForward");
+		float leftRightMovement = Input.GetAxis("MoveRight", "MoveLeft");
+
+		_forwardBackwardMovementSpeed = _forwardBackwardMovementSpeed.Lerp(
+			(forwardBackwardMovement == 0.0f ? 0.0f : forwardBackwardMovement * _maxMovementSpeed),
+			FloatExtensionMethods.DampFactorForLerp(_movementSmoothing, delta));
 		
-		/// <summary>
-		/// Calls movement and rotation methods every frame.
-		/// </summary>
-		public override void _Process(double delta)
+		_leftRightMovementSpeed = _leftRightMovementSpeed.Lerp(
+			(leftRightMovement == 0.0f ? 0.0f : leftRightMovement * _maxMovementSpeed),
+			FloatExtensionMethods.DampFactorForLerp(_movementSmoothing, delta));
+
+		Vector3 combinedMovement = Transform.Basis.Z * _forwardBackwardMovementSpeed
+		                           + Transform.Basis.X * _leftRightMovementSpeed;
+
+		// Needs to be normalized to avoid diagonal movement being faster than single-axis movement.
+		if (combinedMovement.Length() > _maxMovementSpeed)
 		{
-			RotateMainCharacter(delta);
-			MoveMainCharacter(delta);
+			combinedMovement = combinedMovement.Normalized() * _maxMovementSpeed;
 		}
 
-		/// <summary>
-		/// Rotates main character based on last stored relative mouse moment and rotation speed.
-		/// </summary>
-		private void RotateMainCharacter(double delta)
-		{
-			Rotate(Transform.Basis.Y, -_relativeMouseMovement.X * _rotationSpeed * (float)delta);
-			
-			// Needs to be reset as _Input otherwise will never set it to (0.0f, 0.0f).
-			_relativeMouseMovement = new Vector2();
-		}
+		Velocity = combinedMovement * (float)delta;
+		MoveAndSlide();
+	}
 
-		/// <summary>
-		/// Moves main character based on input map.
-		/// </summary>
-		private void MoveMainCharacter(double delta)
-		{
-			// Needs to be normalized to avoid diagonal movements being faster than single-axis movements.
-			Vector3 combinedMovementAxes = (Transform.Basis.Z * Input.GetAxis("MoveBackward", "MoveForward")
-			                                + Transform.Basis.X * Input.GetAxis("MoveRight", "MoveLeft")).Normalized();
-		
-			Velocity = combinedMovementAxes * _movementSpeed * (float)delta;
-			MoveAndSlide();
-		}
+	private void SetDebugIndicatorColor(Color color)
+	{
+		Material debugMaterial = _debugIndicator.GetActiveMaterial(0);
+		StandardMaterial3D newDebugMaterial = debugMaterial.Duplicate() as StandardMaterial3D; 
+		newDebugMaterial.AlbedoColor = color; 
+		_debugIndicator.SetSurfaceOverrideMaterial(0, newDebugMaterial);
 	}
 }
